c5b0963292d10859a9c4fc5a69d233145bc3552e
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2021-05-14 17:12:23 -0500

    Remove printing of providers upon initialization

--- main.go
+++ main.go
@@ -17,10 +17,6 @@ func main() {
 	var providers []provider.Provider
 	providers = append(providers, tda.NewTDAProvider())
 
-	for _, provider := range providers {
-		println(provider.Key())
-	}
-
 	controller := RouteController{providers: providers}
 
 	http.HandleFunc("/authenticate", controller.Authenticate)
