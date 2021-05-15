eddb8cee8c0036f8835aeefe612ac06086cba7ee
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2021-05-15 14:11:23 -0500

    Add randomized state

--- provider/tda/provider.go
+++ provider/tda/provider.go
@@ -1,6 +1,10 @@
 package tda
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/spf13/viper"
@@ -24,12 +28,37 @@ func NewTDAProvider() tdaProvider {
 	}
 }
 
+type oauthState struct {
+	Random   string
+	JWT      string
+	Provider string
+}
+
 func (t tdaProvider) Key() string {
 	return "tda"
 }
 
 func (t tdaProvider) Authenticate(w http.ResponseWriter, req *http.Request, jwtString string, userID string) {
-	url := t.oauth.AuthCodeURL("bloop")
+	b := make([]byte, 48)
+	if _, err := rand.Read(b); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("failed to generate state: %e", err)
+		return
+	}
+
+	state := oauthState{
+		Random:   string(b),
+		JWT:      jwtString,
+		Provider: t.Key(),
+	}
+	data, err := json.Marshal(state)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("failed to marshal state: %e", err)
+		return
+	}
+
+	url := t.oauth.AuthCodeURL(base64.RawURLEncoding.EncodeToString(data))
 
 	http.Redirect(w, req, url, http.StatusFound)
 }
