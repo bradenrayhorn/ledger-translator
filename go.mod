209ef175f42440f96c83ef4eebd20a5415960ae5
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2021-06-22 19:13:33 -0500

    Use TLS for gRPC connection

--- go.mod
+++ go.mod
@@ -5,14 +5,17 @@ go 1.16
 require (
 	github.com/bradenrayhorn/ledger-protos v0.2.0
 	github.com/go-redis/redis/v8 v8.8.2
-	github.com/hashicorp/go-hclog v0.16.1 // indirect
+	github.com/hashicorp/go-hclog v0.16.1
 	github.com/hashicorp/vault v1.7.2
 	github.com/hashicorp/vault/api v1.1.0
 	github.com/hashicorp/vault/sdk v0.2.1-0.20210519002511-48c5544c77f4
+	github.com/johanbrandhorst/certify v1.8.1
 	github.com/joho/godotenv v1.3.0
+	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
 	go.opentelemetry.io/otel v0.20.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
 	google.golang.org/grpc v1.37.1
+	logur.dev/adapter/logrus v0.5.0
 )
--- grpc.go
+++ grpc.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 	"log"
+	"net/url"
+	"strings"
+	"time"
 
 	"github.com/bradenrayhorn/ledger-protos/session"
+	"github.com/johanbrandhorst/certify"
+	"github.com/johanbrandhorst/certify/issuers/vault"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	logrusadapter "logur.dev/adapter/logrus"
 )
 
 type GRPCClient struct {
@@ -13,7 +24,12 @@ type GRPCClient struct {
 }
 
 func NewGRPCClient() GRPCClient {
-	conn, err := grpc.Dial(viper.GetString("grpc_host_auth"), grpc.WithInsecure())
+	certify, _ := createCertify()
+	tlsConfig := &tls.Config{
+		GetClientCertificate: certify.GetClientCertificate,
+		RootCAs:              GetCACertPool(),
+	}
+	conn, err := grpc.Dial(viper.GetString("grpc_host_auth"), grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
 	if err != nil {
 		log.Fatalf("could not connect to grpc auth: %v", err)
 	}
@@ -22,4 +38,61 @@ func NewGRPCClient() GRPCClient {
 	return GRPCClient{
 		session: sessionClient,
 	}
+}
+
+func loadVaultToken() string {
+	tokenBytes, err := ioutil.ReadFile(viper.GetString("vault_token_path"))
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	return strings.TrimSpace(string(tokenBytes))
+}
+
+func createCertify() (*certify.Certify, error) {
+	url, err := url.Parse(viper.GetString("vault_url"))
+	if err != nil {
+		return nil, err
+	}
+	issuer := &vault.Issuer{
+		URL:        url,
+		Mount:      viper.GetString("vault_pki_mount"),
+		AuthMethod: &vault.RenewingToken{Initial: loadVaultToken()},
+		Role:       viper.GetString("vault_pki_role"),
+		TimeToLive: time.Hour * 24,
+	}
+
+	log := logrus.New()
+	log.Level = logrus.DebugLevel
+
+	certify := &certify.Certify{
+		Issuer:      issuer,
+		CommonName:  viper.GetString("vault_pki_cn"),
+		Cache:       certify.NewMemCache(),
+		RenewBefore: time.Minute * 10,
+		Logger:      logrusadapter.New(log),
+	}
+	return certify, nil
+}
+
+var certPool *x509.CertPool
+
+func GetCACertPool() *x509.CertPool {
+	if certPool != nil {
+		return certPool
+	}
+	rootCertPool := x509.NewCertPool()
+	pem, err := ioutil.ReadFile(viper.GetString("ca_cert_path"))
+	if err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+
+	if ok := rootCertPool.AppendCertsFromPEM([]byte(strings.TrimSpace(string(pem)))); !ok {
+		log.Println("failed to append pem")
+		return nil
+	}
+	certPool = rootCertPool
+	return certPool
 }
