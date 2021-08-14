29846a1b171956ac31c99134501cf58234366d6b
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2021-08-14 00:33:54 -0500

    Update transit key name in controller test

--- controller_test.go
+++ controller_test.go
@@ -252,7 +252,7 @@ func (s *ControllerTestSuite) TestCanCallback() {
 	savedToken := s.tokenDB.HGet(context.Background(), "good-user-id", "test")
 	s.Require().Nil(savedToken.Err())
 	var token oauth2.Token
-	decrypted, err := s.vaultClient.Logical().Write("transit/decrypt/my-key", map[string]interface{}{
+	decrypted, err := s.vaultClient.Logical().Write("transit/decrypt/ledger_translator", map[string]interface{}{
 		"ciphertext": savedToken.Val(),
 	})
 	s.Require().Nil(err)
